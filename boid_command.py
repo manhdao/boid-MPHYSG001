6494e31b55632b2f6c37e21d9858f986a259a5f7
Manh Dao <ducmanhdao92@gmail.com>
2017-02-21 19:11:58 +0000

    Break down functions into small files

---
+++ boid_command.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+from argparse import ArgumentParser
+from boids import update_boids
+from simulate import simulate
+import yaml
+
+
+with open('config.yaml', 'rb') as f:
+    conf = yaml.load(f.read())    # load the config file
+
+flock_params = conf['flock_params']
+boid_params = conf['boid_params']
+animation_params = conf['animation_params']
+
+#boid_params
+min_x_position = boid_params['min_x_position']
+max_x_position= boid_params['max_x_position']
+min_y_position= boid_params['min_y_position']
+max_y_position = boid_params['max_y_position']
+min_x_velocity = boid_params['min_x_velocity']
+max_x_velocity = boid_params['max_x_velocity']
+min_y_velocity = boid_params['min_y_velocity']
+max_y_velocity = boid_params['max_y_velocity']
+
+#flock_params
+boid_count = flock_params['boid_count']
+fly_middle_strength = flock_params['fly_middle_strength']
+nearby_distance = flock_params['nearby_distance']
+formation_distance = flock_params['formation_distance']
+speed_formation_strength = flock_params['speed_formation_strength']
+
+#generate boids
+boids_x = (np.ones(boid_count)*min_x_position + np.random.rand(1, boid_count)*(max_x_position-min_x_position))
+boids_y = (np.ones(boid_count)*min_y_position + np.random.rand(1, boid_count)*(max_y_position-min_y_position))
+boid_x_velocities=(np.ones(boid_count)*min_x_velocity + np.random.rand(1, boid_count)*(max_x_velocity-min_x_velocity))
+boid_y_velocities=(np.ones(boid_count)*min_y_velocity + np.random.rand(1, boid_count)*(max_y_velocity-min_y_velocity))
+
+boids = np.concatenate((boids_x,boids_y,boid_x_velocities,boid_y_velocities),axis = 0)
+
+if __name__ == "__main__":
+
+
+
+	simulate(animation_params, boids)
+
+
+
+
+
+
--- boids.py
+++ boids.py
@@ -4,45 +4,6 @@ import numpy as np
 import random
 import yaml
 
-with open('config.yaml', 'rb') as f:
-    conf = yaml.load(f.read())    # load the config file
-
-flock_params = conf['flock_params']
-boid_params = conf['boid_params']
-animation_params = conf['animation_params']
-
-#boid_params
-min_x_position = boid_params['min_x_position']
-max_x_position= boid_params['max_x_position']
-min_y_position= boid_params['min_y_position']
-max_y_position = boid_params['max_y_position']
-min_x_velocity = boid_params['min_x_velocity']
-max_x_velocity = boid_params['max_x_velocity']
-min_y_velocity = boid_params['min_y_velocity']
-max_y_velocity = boid_params['max_y_velocity']
-
-#flock_params
-boid_count = flock_params['boid_count']
-fly_middle_strength = flock_params['fly_middle_strength']
-nearby_distance = flock_params['nearby_distance']
-formation_distance = flock_params['formation_distance']
-speed_formation_strength = flock_params['speed_formation_strength']
-
-#aniation params
-axes_min = animation_params['axes_min']
-axes_max = animation_params['axes_max']
-frames = animation_params['frames']
-interval = animation_params['interval']
-
-
-# Deliberately terrible code for teaching purposes
-boids_x = (np.ones(boid_count)*min_x_position + np.random.rand(1, boid_count)*(max_x_position-min_x_position))
-boids_y = (np.ones(boid_count)*min_y_position + np.random.rand(1, boid_count)*(max_y_position-min_y_position))
-boid_x_velocities=(np.ones(boid_count)*min_x_velocity + np.random.rand(1, boid_count)*(max_x_velocity-min_x_velocity))
-boid_y_velocities=(np.ones(boid_count)*min_y_velocity + np.random.rand(1, boid_count)*(max_y_velocity-min_y_velocity))
-
-boids = np.concatenate((boids_x,boids_y,boid_x_velocities,boid_y_velocities),axis = 0)
-
 
 def update_boids(boids):
     xs,ys,xvs,yvs=boids
@@ -87,18 +48,3 @@ def update_boids(boids):
     # Move according to velocities
     xs += xvs
     ys += yvs
-
-figure=plt.figure()
-axes=plt.axes(xlim=(axes_min,axes_max), ylim=(axes_min,axes_max))
-scatter=axes.scatter(boids[0],boids[1])
-
-def animate(frame):
-   update_boids(boids)
-   boid_zip = list(zip(boids[0],boids[1]))
-   scatter.set_offsets(boid_zip)
-
-
-anim = animation.FuncAnimation(figure, animate, frames=frames, interval=interval)
-
-if __name__ == "__main__":
-	plt.show()
