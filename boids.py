02c9104fa68cfea1b3578c329bdb455307eb517f
Manh Dao <ducmanhdao92@gmail.com>
2017-02-24 02:45:34 +0000

    Put the function inside a class, update tests

--- boids.py
+++ boids.py
@@ -1,76 +1,127 @@
 from matplotlib import pyplot as plt
 from matplotlib import animation
 import numpy as np
-import random
-import yaml
-
-with open('config.yaml', 'rb') as f:
-    conf = yaml.load(f.read())    # load the config file
-
-flock_params = conf['flock_params']
-boid_params = conf['boid_params']
-
-#flock_params
-boid_count = flock_params['boid_count']
-fly_middle_strength = flock_params['fly_middle_strength']
-nearby_distance = flock_params['nearby_distance']
-formation_distance = flock_params['formation_distance']
-speed_formation_strength = flock_params['speed_formation_strength']
 
 
+class Flock(object):
+	def __init__(self, flock_params, boid_params):
+		"""Initialise a flock with boid and flock information"""
+		
+		self.boid_count = flock_params['boid_count']
+		self.fly_middle_strength = flock_params['fly_middle_strength']
+		self.nearby_distance = flock_params['nearby_distance']
+		self.formation_distance = flock_params['formation_distance']
+		self.speed_formation_strength = flock_params['speed_formation_strength']
 
 
-def fly_middle(positions, velocities):
-	positions=np.asarray(positions)
-	velocities=np.asarray(velocities)
+		lower_pos_limit = np.array([boid_params['min_x_position'],boid_params['min_y_position']])
+		upper_pos_limit = np.array([boid_params['max_x_position'],boid_params['max_y_position']])
+		lower_vel_limit = np.array([boid_params['min_x_velocity'],boid_params['min_y_velocity']])
+		upper_vel_limit = np.array([boid_params['max_x_velocity'],boid_params['max_y_velocity']])
 
-	# Fly towards the middle
-	middle = np.mean(positions, 1)
-	direction_to_middle = positions - middle[:,np.newaxis]
-	velocities -= direction_to_middle * fly_middle_strength
+		self.positions = lower_pos_limit[:,np.newaxis] + \
+		np.random.rand(2, self.boid_count)*(upper_pos_limit - lower_pos_limit)[:,np.newaxis]
+		self.velocities = lower_vel_limit[:,np.newaxis] + \
+		np.random.rand(2, self.boid_count)*(upper_vel_limit - lower_vel_limit)[:,np.newaxis]
 
 
-def fly_away(positions, velocities):
-	positions=np.asarray(positions)
-	velocities=np.asarray(velocities)
+	#I define helper functions so that Flock can call fly_middle or fly_away if need to
 
-	# Fly away from nearby boids
-	separations = positions[:,np.newaxis,:] - positions[:,:,np.newaxis]
-	distances = separations * separations
-	sum_distances = np.sum(distances, 0)
+	def fly_middle(self):
+		fly_middle_helper(self.positions,self.velocities,self.fly_middle_strength)
 
-	far_away = sum_distances > nearby_distance
-	separations_if_close = np.copy(separations)
-	separations_if_close[0,:,:][far_away]= 0
-	separations_if_close[1,:,:][far_away]= 0
+	
+	@staticmethod
+	def fly_middle_helper(positions, velocities, fly_middle_strength):
 
-	velocities += np.sum(separations_if_close,1)
+		# Fly towards the middle
+		middle = np.mean(positions, 1)
+		direction_to_middle = positions - middle[:,np.newaxis]
+		velocities -= direction_to_middle * fly_middle_strength
 
 
 
-def match_speed(positions, velocities):
-	positions=np.asarray(positions)
-	velocities=np.asarray(velocities)
+	def fly_away(self):
+		fly_away_helper(self.positions, self.velocities, self.nearby_distance)
 
-	# Try to match speed with nearby boids
-	separations = positions[:,np.newaxis,:] - positions[:,:,np.newaxis]
-	distances = separations * separations
-	sum_distances = np.sum(distances, 0)
+	
+	@staticmethod
+	def fly_away_helper(positions, velocities, nearby_distance):
+		positions=np.asarray(positions)
+		velocities=np.asarray(velocities)
 
-	formations_far = sum_distances > formation_distance
-	velocity_difference = velocities[:,np.newaxis,:] - velocities[:,:,np.newaxis]
+		# Fly away from nearby boids
+		separations = positions[:,np.newaxis,:] - positions[:,:,np.newaxis]
+		distances = separations * separations
+		sum_distances = np.sum(distances, 0)
 
-	velocity_if_close = np.copy(velocity_difference)
-	velocity_if_close[0,:,:][formations_far]= 0
-	velocity_if_close[1,:,:][formations_far]= 0
+		far_away = sum_distances > nearby_distance
+		separations_if_close = np.copy(separations)
+		separations_if_close[0,:,:][far_away]= 0
+		separations_if_close[1,:,:][far_away]= 0
 
-	velocities -= np.mean(velocity_if_close, 1) * speed_formation_strength
-
-	positions += velocities
-
+		velocities += np.sum(separations_if_close,1)
 
 
-def update_boids(positions, velocities):
-    fly_middle(positions, velocities)
-    fly_away(positions, velocities)
-    match_speed(positions, velocities)
+
+	def match_speed(self):
+		match_speed_helper(self.positions, self.velocities, 
+						self.formation_distance, self.speed_formation_strength)
+
+	
+	@staticmethod
+	def match_speed_helper(positions, velocities, formation_distance, speed_formation_strength):
+		positions=np.asarray(positions)
+		velocities=np.asarray(velocities)
+
+		# Try to match speed with nearby boids
+		separations = positions[:,np.newaxis,:] - positions[:,:,np.newaxis]
+		distances = separations * separations
+		sum_distances = np.sum(distances, 0)
+
+		formations_far = sum_distances > formation_distance
+		velocity_difference = velocities[:,np.newaxis,:] - velocities[:,:,np.newaxis]
+
+		velocity_if_close = np.copy(velocity_difference)
+		velocity_if_close[0,:,:][formations_far]= 0
+		velocity_if_close[1,:,:][formations_far]= 0
+
+		velocities -= np.mean(velocity_if_close, 1) * speed_formation_strength
+
+		positions += velocities
+
+
+
+	def update_boids(self):
+		self.update_bodis_helper(self.positions, self.velocities, 
+							self.fly_middle_strength, self.nearby_distance, 
+							self.formation_distance, self.speed_formation_strength)
+		
+
+	def update_boids_helper(positions, velocities, 
+					fly_middle_strength, nearby_distance, formation_distance, speed_formation_strength):
+		Flock.fly_middle_helper(positions, velocities, fly_middle_strength)
+		Flock.fly_away_helper(positions, velocities, nearby_distance)
+		Flock.match_speed_helper(positions, velocities, formation_distance, speed_formation_strength)
+
+
+		
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- test_boid.py
+++ test_boid.py
@@ -1,35 +1,24 @@
-import boids as bs
+from boids import Flock
 from nose.tools import assert_equal
 import os
 import numpy as np
 import yaml
+from unittest.mock import patch, MagicMock
 
 
-def test_update_boids():
-	#Test the update_boids function
-    boid_data=yaml.load(open(
-        os.path.join(os.path.dirname(__file__),
-        'fixture\\update_boid_fixture.yaml')))
-    before=np.asarray(boid_data['before'])
-    pos = before[0:2,:]
-    vel = before[2:4,:]
-    bs.update_boids(pos,vel)
-    after = np.concatenate((pos, vel),axis=0)
-
-    #Test 2 particular numbers in the 2 arrays
-    assert_equal(np.asarray(boid_data['after'])[3,8],after[3,8])
-    assert_equal(np.asarray(boid_data['after'])[0,6],after[0,6])
-
-
+#@patch.object(Flock'boids.Flock')
 def test_fly_middle():
-	#Test the fly_middle function
+	"""Test the fly_middle function"""
+
 	boid_data=yaml.load(open(
         os.path.join(os.path.dirname(__file__),
         'fixture\\fly_middle_fixture.yaml')))
 	before=np.asarray(boid_data['before'])
 	pos = before[0:2,:]
 	vel = before[2:4,:]
-	bs.fly_middle(pos,vel)
+
+	Flock.fly_middle_helper(pos,vel,fly_middle_strength = 0.01)
+
 	after = np.concatenate((pos, vel),axis=0)
 
 	#Test 2 particular numbers in the 2 velocity arrays
@@ -45,7 +34,7 @@ def test_fly_away():
 	before=np.asarray(boid_data['before'])
 	pos = before[0:2,:]
 	vel = before[2:4,:]
-	bs.fly_away(pos,vel)
+	Flock.fly_away_helper(pos,vel,100)
 	after = np.concatenate((pos, vel),axis=0)
 
 	#Test 2 particular numbers in the 2 velocity arrays
@@ -61,9 +50,26 @@ def test_match_speed():
 	before=np.asarray(boid_data['before'])
 	pos = before[0:2,:]
 	vel = before[2:4,:]
-	bs.match_speed(pos,vel)
+	Flock.match_speed_helper(pos,vel,10000,0.125)
 	after = np.concatenate((pos, vel),axis=0)
 
 	#Test 2 particular numbers in the 2 arrays
 	assert_equal(np.asarray(boid_data['after'])[3,25],after[3,25])
 	assert_equal(np.asarray(boid_data['after'])[0,7],after[0,7])
+
+
+def test_update_boids():
+	#Test the update_boids function
+    boid_data=yaml.load(open(
+        os.path.join(os.path.dirname(__file__),
+        'fixture\\update_boid_fixture.yaml')))
+    before=np.asarray(boid_data['before'])
+    pos = before[0:2,:]
+    vel = before[2:4,:]
+    
+    Flock.update_boids_helper(pos,vel,0.01,100,10000,0.125)
+    after = np.concatenate((pos, vel),axis=0)
+
+    #Test 2 particular numbers in the 2 arrays
+    assert_equal(np.asarray(boid_data['after'])[3,8],after[3,8])
+    assert_equal(np.asarray(boid_data['after'])[0,6],after[0,6])
